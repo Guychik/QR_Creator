a7201f3e6a72f55b93385634c383728738d884b2
GuyChik <guyg1244@gmail.com>
2022-01-30 17:43:15 +0200

    made an offline version

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+from create_qr import create_qr
 
 app = Flask(__name__)
 
@@ -9,7 +10,8 @@ def index():
 @app.route('/render', methods=['GET', 'POST'])
 def index2():
     hash = request.args.get('hash')
-    return render_template("website.html", qr = 'https://api.qrserver.com/v1/create-qr-code/?data={}&amp;size=500x500'.format(hash))
+    create_qr(hash)
+    return render_template("website.html", qr = "static/images/qr.png")
 
 if __name__ == '__main__':
     # run app in debug mode on port 5000
